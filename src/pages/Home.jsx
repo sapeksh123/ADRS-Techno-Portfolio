0ed0bed48c36880578b0f3e0ee0002be3d9628fc
sapeksh <sapekshvishwakarma@gmail.com>
2025-09-28 19:25:24 +0530

    carousel add in Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,9 @@
 import React from "react";
 import Hero from "../components/Hero";
 // import ServiceCard from "../components/ServiceCard";
-// import PartnersCarousel from "../components/PartnersCarousel";
+import PartnersCarousel from "../components/PartnersCarousel";
 // import ProjectCard from "../components/ProjectCard";
-// import { motion } from "framer-motion";
+import { motion } from "framer-motion";
 // import TeamCard from "../components/TeamCard";
 
 // Motion Variants for reusability
@@ -20,37 +20,44 @@ const scaleIn = (delay = 0) => ({
   whileInView: { opacity: 1, scale: 1 },
   transition: { delay, duration: 0.5 },
 });
+import logo1 from "/swws.png";
+import logo2 from "/twocups.png";
+import logo3 from "/jip.png";
+import logo4 from "/classcrew.png";
+import logo5 from "/videocrew.png";
+import logo6 from "/rays.png";
+import logo7 from "/devcode.jpg";
+import logo8 from "/Event.png";
+
+// Example animation functions
+
 
 export default function Home() {
+     const partners = [logo1, logo2, logo3, logo4, logo5, logo6, logo7,logo8];
   return (
     <div>
       {/* Hero Section */}
       <Hero />
 
       
+{/* Divider */}
 
-      {/* Divider */}
-      <div className="w-full h-1 bg-gradient-to-r from-brand-700/10 via-gray-200 to-brand-700/10" />
+{/* Partners Section */}
+<section className="container mx-auto px-4 py-20 ">
+  <motion.h3
+    {...fadeUp()}
+    className="text-4xl font-extrabold text-center text-gray-800"
+  >
+    Our Partners
+  </motion.h3>
+  <motion.div {...scaleIn(0.3)} className="mt-12">
+    <PartnersCarousel partners={partners} />
+  </motion.div>
+</section>
 
+{/* Divider */}
+<div className="w-full h-1 bg-gradient-to-r from-gray-600/30 via-gray-700/30 to-gray-600/30" />
 
-      {/* Divider */}
-      <div className="w-full h-1 bg-gradient-to-r from-brand-700/10 via-gray-200 to-brand-700/10" />
-
-      {/* Partners Section */}
-      {/* <section className="container mx-auto px-4 py-20">
-        <motion.h3
-          {...fadeUp()}
-          className="text-4xl font-extrabold text-center text-brand-900"
-        >
-          Our Partners
-        </motion.h3>
-        <motion.div {...scaleIn(0.3)} className="mt-12">
-          <PartnersCarousel partners={partners} />
-        </motion.div>
-      </section> */}
-
-      {/* Divider */}
-      <div className="w-full h-1 bg-gradient-to-r from-brand-700/10 via-gray-200 to-brand-700/10" />
 
     </div>
   );
