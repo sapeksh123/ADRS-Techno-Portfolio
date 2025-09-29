e2da55429213213bdc4b7528205228c34f2c0f65
sapeksh <sapekshvishwakarma@gmail.com>
2025-09-29 12:48:47 +0530

    fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,3 @@
-
-
 import React from "react";
 import Hero from "../components/Hero";
 // import ServiceCard from "../components/ServiceCard";
@@ -8,7 +6,6 @@ import PartnersCarousel from "../components/PartnersCarousel";
 import { motion } from "framer-motion";
 // import TeamCard from "../components/TeamCard";
 
-// Motion Variants for reusability
 const fadeUp = (delay = 0) => ({
   initial: { opacity: 0, y: 40 },
   whileInView: { opacity: 1, y: 0 },
@@ -28,39 +25,39 @@ import logo5 from "/videocrew.png";
 import logo6 from "/rays.png";
 import logo7 from "/devcode.jpg";
 import logo8 from "/Event.png";
-
-// Example animation functions
-
+import logo9 from "/Event.png";
+import logo10 from "/Event.png";
 
 export default function Home() {
-     const partners = [logo1, logo2, logo3, logo4, logo5, logo6, logo7,logo8];
+  const partners = [
+    logo1,
+    logo2,
+    logo3,
+    logo4,
+    logo5,
+    logo6,
+    logo7,
+    logo8,
+    logo9,
+    logo10,
+  ];
   return (
     <div>
-      {/* Hero Section */}
       <Hero />
 
-      
-{/* Divider */}
+      <section className="container mx-auto px-4 py-20">
+        <motion.h3
+          {...fadeUp()}
+          className="text-4xl font-extrabold text-center text-gray-800"
+        >
+          Our Partners
+        </motion.h3>
 
-{/* Partners Section */}
-<section className="container mx-auto px-4 py-20 ">
-  <motion.h3
-    {...fadeUp()}
-    className="text-4xl font-extrabold text-center text-gray-800"
-  >
-    Our Partners
-  </motion.h3>
-  <motion.div {...scaleIn(0.3)} className="mt-12">
-    <PartnersCarousel partners={partners} />
-  </motion.div>
-</section>
-
-{/* Divider */}
-<div className="w-full h-1 bg-gradient-to-r from-gray-600/30 via-gray-700/30 to-gray-600/30" />
-
-
+        <motion.div {...scaleIn(0.3)} className="mt-12">
+          <PartnersCarousel partners={partners} />
+        </motion.div>
+      </section>
+      <div className="w-full h-1 bg-gradient-to-r from-gray-600/30 via-gray-700/30 to-gray-600/30" />
     </div>
   );
 }
-
-
